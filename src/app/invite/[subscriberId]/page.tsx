558fbd86e213e92c0c5b0ff25880e604ce9fc7f4
ProgAttack <gianramiress@hotmail.com>
2025-02-20 10:55:55 -0300

    backend connection and implemented API requests

--- src/app/invite/[subscriberId]/page.tsx
+++ src/app/invite/[subscriberId]/page.tsx
@@ -1,11 +1,21 @@
 import Image from 'next/image'
-import logo from '../../assets/logo.svg'
+import logo from '../../../assets/logo.svg'
 import { InviteLinkInput } from './invite-link-input'
 import { Ranking } from './ranking'
 import { Stats } from './stats'
 
-export default function InvitePage() {
-  const invateLink = 'http://localhost:3000/invite/d123125j312kl'
+interface InvitePageProps {
+  params: Promise<{
+    subscriberId: string
+  }>
+}
+
+
+
+
+export default async function InvitePage(props: InvitePageProps) {
+  const {subscriberId} = await props.params
+  const invateLink = `http://localhost:3333/invites/${subscriberId}`
   return (
     <div className="min-h-dvh flex items-center justify-between gap-16 flex-col md:flex-row">
       <div className="flex flex-col gap-10 w-full max-w-[550px]">
@@ -30,7 +40,7 @@ export default function InvitePage() {
             </p>
           </div>
           <InviteLinkInput inviteLink={invateLink} />
-          <Stats />
+          <Stats subscriberId = {subscriberId}/>
         </div>
       </div>
       <Ranking />
